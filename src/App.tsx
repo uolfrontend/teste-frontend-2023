f26a152e2f8f484a3d43852c25962ec961f2c5ac
Henry Franklin <henrymais@gmail.com>
2024-02-29 23:22:20 -0300

    refact: implements style folder context

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Styles/Main.scss';
+
+function App() {
+  return (
+    <div className='main-app'>
+      <header className='header'>
+        <p>Main Area</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
