beca334dddfec44aec59d6a6f873c995002943f5
Henry Franklin <henrymais@gmail.com>
2024-02-29 23:22:20 -0300

    feat: add table component

---
+++ src/Components/Table/Containers/types.ts
@@ -0,0 +1,22 @@
+interface Field {
+  label?: string;
+  isVisible: boolean;
+}
+
+export type FieldsHeader = {
+  [fieldName: string]: Field;
+};
+
+export type Item = {
+  id: string | number;
+  [fieldName: string]: any;
+};
+
+export interface TableRowsProps {
+  fields: FieldsHeader;
+  items: Array<Item>;
+}
+
+export interface TableHeaderProps {
+  fields: FieldsHeader;
+}
